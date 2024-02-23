80fd893c31ca7dad855812964647397fe80fcb9f
wxz <759126349@qq.com>
2024-02-23 16:40:02 +0800

    设置请求参数

--- miniprogram/utils/request.js
+++ miniprogram/utils/request.js
@@ -1,9 +1,26 @@
 class WxRequest {
 
-  constructor() {
+  // 默认请求参数
+  defaults = {
+    baseURL: '',
+    url: '',
+    data: null,
+    method: 'GET',
+    header: {
+      "Content-type": 'application/json'
+    },
+    timeout: 60000
+  }
+
+  constructor(params = {}) {
+    this.defaults = Object.assign({}, this.defaults, params)
   }
 
   request(options) {
+
+    options.url = this.defaults.baseURL = options.url
+    options = { ...this.defaults, ...options }
+
     return new Promise((resolve, reject) => {
       wx.request({
         ...options,
@@ -17,3 +34,10 @@ class WxRequest {
     })
   }
 }
+
+const instance = new WxRequest({
+  baseURL: 'https://gmall-prod.atguigu.cn/mall-api',
+  timeout: 15000
+})
+
+export default instance
