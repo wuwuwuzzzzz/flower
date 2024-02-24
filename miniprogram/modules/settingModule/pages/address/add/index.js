9b373dd7d18b5a8d70dfd4660fc17fbd2d449f8a
wxz <759126349@qq.com>
2024-02-24 23:57:02 +0800

    拒绝授权和授权完整流程

--- miniprogram/modules/settingModule/pages/address/add/index.js
+++ miniprogram/modules/settingModule/pages/address/add/index.js
@@ -23,11 +23,18 @@ Page({
     // 完整地址
     fullAddress: '',
     // 是否设置为默认地址
-    isDefault: 0
+    isDefault: false
   },
 
   // 保存收货地址
   saveAddressForm(event) {
+
+    const { provinceName, cityName, districtName, address, isDefault } = this.data
+    const params = {
+      ...this.data,
+      fullAddress: provinceName + cityName + districtName + address,
+      isDefault: isDefault ? 1 : 0
+    }
   },
 
   // 省市区选择
@@ -44,6 +51,44 @@ Page({
       cityCode,
       districtCode
     })
+  },
+
+  // 获取用户地理位置信息
+  async onLocation() {
+    const res = await wx.getLocation()
+    console.log(res)
+  },
+
+  // wx.getLocation 获取用户地理位置
+  async getLocation() {
+
+    // 获取用户所有的授权信息
+    const { authSetting } = await wx.getSetting()
+
+    if (authSetting['scope.userLocation']) {
+      try {
+        const res = await wx.getLocation({})
+      } catch (err) {
+        wx.toast({ title: '您拒绝授权获取位置信息' })
+      }
+    } else {
+      const modalRes = wx.modal({
+        title: '授权提示',
+        content: '需要获取地理位置信息，请确认授权'
+      })
+
+      if (!modalRes) return wx.toast({ title: '您拒绝了授权' })
+
+      const { authSetting } = await wx.openSetting()
+
+      if (!authSetting['scope.userLocation']) return wx.toast({ title: '授权失败' })
+
+      try {
+        await wx.getLocation({})
+      } catch (err) {
+        wx.toast({ title: '您拒绝授权获取位置信息' })
+      }
+    }
   }
 
 })
