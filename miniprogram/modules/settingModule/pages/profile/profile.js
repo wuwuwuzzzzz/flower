7c2cead5c3eaa231206dd164b7f7453b613d751d
wxz <759126349@qq.com>
2024-02-24 18:48:16 +0800

    渲染用户信息

--- miniprogram/modules/settingModule/pages/profile/profile.js
+++ miniprogram/modules/settingModule/pages/profile/profile.js
@@ -1,6 +1,11 @@
 // pages/profile/profile.js
 
+import { userBehavior } from './behavior';
+
 Page({
+
+  behaviors: [userBehavior],
+
   // 页面的初始数据
   data: {
     isShowPopup: false // 控制更新用户昵称的弹框显示与否
