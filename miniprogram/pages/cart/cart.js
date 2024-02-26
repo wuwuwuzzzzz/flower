d41210e2ba98ab7422f1494a576ca94e4210f3b8
wxz <759126349@qq.com>
2024-02-26 16:42:46 +0800

    获取并渲染购物车列表

--- miniprogram/pages/cart/cart.js
+++ miniprogram/pages/cart/cart.js
@@ -1,16 +1,46 @@
-// pages/cart/component/cart.js
-Component({
+import { ComponentWithStore } from 'mobx-miniprogram-bindings';
+import { userStore } from '@/stores/userStore';
+import { reqCartList } from '../../api/cart';
+
+ComponentWithStore({
 
   // 组件的属性列表
-  properties: {},
+  storeBindings: {
+    store: userStore,
+    fields: ['token']
+  },
 
   // 组件的初始数据
   data: {
-    cartList: [1, 2, 3, 4],
+    cartList: [],
     emptyDes: '还没有添加商品，快去添加吧～'
   },
 
   // 组件的方法列表
-  methods: {}
+  methods: {
+    // 展示文案信息
+    async showTipGetList() {
+      const { token } = this.data
+      if (!token) {
+        this.setData({
+          emptyDes: '您尚未登录，点击登录获取更多权益',
+          cartList: []
+        })
+      } else {
+        const { code, data: cartList } = await reqCartList()
+        console.log(cartList)
+        if (code === 200) {
+          this.setData({
+            cartList,
+            emptyDes: cartList.length === 0 && '还没有添加商品，快去添加吧～'
+          })
+        }
+      }
+    },
+
+    onShow() {
+      this.showTipGetList()
+    }
+  }
 
 })
