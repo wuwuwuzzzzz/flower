9ba750bea96183c4f6585c13e33270526d90ddf7
wxz <759126349@qq.com>
2024-02-26 11:46:41 +0800

    删除收货地址

--- miniprogram/modules/settingModule/pages/address/list/index.js
+++ miniprogram/modules/settingModule/pages/address/list/index.js
@@ -1,11 +1,26 @@
 // pages/address/list/index.js
-import { reqAddressList } from '../../../../../api/address';
+import { reqAddressList, reqDelAddress } from '../../../../../api/address';
 
 Page({
 
   // 页面的初始数据
   data: {
     addressList: []
+  },
+
+  async delAddress(event) {
+
+    const { id } = event.currentTarget.dataset
+
+    const modalRes = await wx.modal({
+      content: '您确认删除该收货地址吗？'
+    })
+
+    if (modalRes) {
+      await reqDelAddress(id)
+      wx.toast({ title: '收货地址删除成功' })
+      await this.getAddressList()
+    }
   },
 
   // 去编辑页面
