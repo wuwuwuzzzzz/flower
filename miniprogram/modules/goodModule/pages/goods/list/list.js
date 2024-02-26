18540a33e653208673298c95c6c2b7a41cd04213
wxz <759126349@qq.com>
2024-02-26 14:27:37 +0800

    获取商品列表数据

--- miniprogram/modules/goodModule/pages/goods/list/list.js
+++ miniprogram/modules/goodModule/pages/goods/list/list.js
@@ -1,13 +1,38 @@
 // pages/goods/list/index.js
 
+import { reqGoodsList } from '../../../../../api/goods';
+
 Page({
 
   // 页面的初始数据
   data: {
     // 商品列表数据
     goodsList: [],
+    // 数据总条数
+    total: 0,
     // 判断数据是否加载完毕
-    isFinish: false
+    isFinish: false,
+    // 商品列表请求参数
+    requestData: {
+      page: 1,
+      limit: 10,
+      category1Id: '',
+      category2Id: ''
+    }
+  },
+
+  // 获取商品列表数据
+  async getGoodsList() {
+    const { data } = await reqGoodsList(this.data.requestData)
+    this.setData({
+      goodsList: data.records,
+      total: data.total
+    })
+  },
+
+  onLoad(options) {
+    Object.assign(this.data.requestData, options)
+    this.getGoodsList()
   }
 
 })
