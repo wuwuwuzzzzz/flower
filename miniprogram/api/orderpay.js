b7eb8950507eb4ec64438e6f9ccdbc0662e99e8a
wxz <759126349@qq.com>
2024-02-27 11:04:18 +0800

    订单结算接口 API

---
+++ miniprogram/api/orderpay.js
@@ -0,0 +1,31 @@
+import http from '@/utils/http';
+
+// 获取订单详情
+export const reqOrderInfo = () => {
+  return http.get('/order/trade')
+}
+
+// 获取订单详情页面的收货地址
+export const reqOrderAddress = () => {
+  return http.get('/userAddress/getOrderAddress')
+}
+
+// 获取立即购买商品的详细信息
+export const reqBuyNowGood = ({ goods, ...data }) => {
+  return http.get(`/order/buy/${goodsId}`, data)
+}
+
+// 提交订单
+export const reqOrderSubmit = () => {
+  return http.post('/order/submitOrder')
+}
+
+// 获取微信预支付信息
+export const reqPrePayInfo = (orderNo) => {
+  return http.get(`/wechat/createJsapi/{orderNo}`)
+}
+
+// 获取微信支付结果
+export const reqPayStatus = (orderNo) => {
+  return http.get(`/wechat/queryPayStatus/{orderNo}`)
+}
